483f1b98eba0aa9149c96b8aa489bf89742fe266
Adhya-accolite <77055950+Adhya-accolite@users.noreply.github.com>
2021-01-26 18:35:00 +0530

    Update School.java

--- Hibernate-Evening/hibernatespring/src/main/java/com/accolit/hibernatespring/entities/School.java
+++ Hibernate-Evening/hibernatespring/src/main/java/com/accolit/hibernatespring/entities/School.java
@@ -1,37 +1,46 @@
 package com.accolit.hibernatespring.entities;
 
-import java.util.List;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import lombok.Data;
 
 @Entity
+@Data
 public class School {
 	 @Id
 	    @GeneratedValue(strategy = GenerationType.IDENTITY)
-	    private Integer id;
+	    private Integer school_id;
+	 
 	 @Column
-	    private List<Quiz> quiz;
+	 @ElementCollection
+	 @OneToMany(targetEntity=Quiz.class, mappedBy="school", cascade = CascadeType.ALL,fetch=FetchType.EAGER)
+	    private Set<Quiz> quiz;
 
 	    @Column
 	    private String Address;
 
 	    public Integer getId() {
-		return id;
+		return school_id;
 	}
 
-	public void setId(Integer id) {
-		this.id = id;
+	public void setId(Integer school_id) {
+		this.school_id = school_id;
 	}
-
-	public List<Quiz> getQuiz() {
+	
+	public Set<Quiz> getQuiz() {
 		return quiz;
 	}
 
-	public void setQuiz(List<Quiz> quiz) {
+	public void setQuiz(Set<Quiz> quiz) {
 		this.quiz = quiz;
 	}
 
