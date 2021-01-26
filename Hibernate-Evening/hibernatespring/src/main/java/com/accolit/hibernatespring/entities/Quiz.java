6f16e56fbab96fad2d98b2217335ea413cb1ffab
Adhya-accolite <77055950+Adhya-accolite@users.noreply.github.com>
2021-01-26 18:36:56 +0530

    Update Quiz.java

--- Hibernate-Evening/hibernatespring/src/main/java/com/accolit/hibernatespring/entities/Quiz.java
+++ Hibernate-Evening/hibernatespring/src/main/java/com/accolit/hibernatespring/entities/Quiz.java
@@ -1,28 +1,42 @@
 package com.accolit.hibernatespring.entities;
 
-import java.util.List;
+import java.util.Set;
+
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+
 
 @Entity(name="quiz")
-
+@Data
 public class Quiz {
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @OneToOne
     private Integer id;
 
     @Column
     private String Question;
 
     @Column
-    private List<Option> options;
-	 public Integer getId() {
+    private Set<String> options;
+    private School school;
+    @ManyToOne
+    @JoinColumn(name="school_id")
+    public School getSchool() {
+        return school;
+    }
+    public void setCollege(School school) {
+        this.school = school;
+    }
+
+	public Integer getId() {
 		return id;
 	}
 
@@ -38,13 +52,14 @@ public class Quiz {
 		Question = question;
 	}
 
-	public List<Option> getOptions() {
+	public Set<String> getOptions() {
 		return options;
 	}
 
-	public void setOptions(List<Option> options) {
+	public void setOptions(Set<String> options) {
 		this.options = options;
 	}
+	
 
 	
 
